4791e4cc1557e9fe0aad4dd6e7f1f88767d401b8
YoonjinXD <45695271+YoonjinXD@users.noreply.github.com>
2019-08-27 12:16:43 +0900

    download.sh 잘못된 부분 수정

--- download.sh
+++ download.sh
@@ -3,10 +3,11 @@ mkdir data/translation
 mkdir data/translation/train
 mkdir data/translation/test
 
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en data/translation/train
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de data/translation/train
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.en data/translation/test
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.de data/translation/test
+cd data/translation/train
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de
+cd ../test
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.en
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.de
 
 echo "Data has downloaded."
-
